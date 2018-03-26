514bf3b6aefeee8c611a3f3ffe8758083f62f5a8
Andrey Lozhkin <las@htc-cs.ru>
2018-03-26 11:07:13 +0400

    Encode url but can't decode it yet

--- index.js
+++ index.js
@@ -14,8 +14,30 @@ app.use((req, res, next) => {
 });
 
 app.post('/shorten', (req, res) => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const base = alphabet.length;
+    const longUrl = 'qweqwe';
+    let shortUrl = '';
+    let id = 5000;
+    while (id) {
+        shortUrl += (alphabet[id%62]);
+        id = Math.round(id/62);
+    }
+    console.log('shortUrl', shortUrl);
+    console.log('decode(shortUrl)', decode(shortUrl));
     console.log('req.body', req.body);
 });
+
+function decode(shortURL){
+    let id = 0; // initialize result
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const base = alphabet.length;
+
+    // A simple base conversion logic
+    // shortURL.split('').forEach((letter) => id = alphabet.split('').findIndex((item) => item === letter) + id * 62);
+
+    return id;
+}
 
 
 app.listen(port, () => console.log(`Listening to ${port}`));
