dbc401de0e859fff41b8514f9389cbd19e3b350c
Andrey Lozhkin <las@htc-cs.ru>
2018-03-28 11:44:04 +0400

    Feat: add frontend and move port and host to consts

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const encode = require('./helpers/decodeEncode').encode;
 const decode = require('./helpers/decodeEncode').decode;
 const getNextSequence = require('./helpers/getNextSequence');
 const app = express();
+const host = "127.0.0.1";
 const port = 3000;
 
+app.use(express.static('static'));
 app.use((req, res, next) => {
     let body = [];
     req.on('data', (chunk) => {
@@ -20,13 +22,17 @@ app.use((req, res, next) => {
 });
 
 app.post('/shorten', async (req, res) => {
-    const { link } = req.body;
-    const connection = await MongoClient.connect('mongodb://Andrew:ANDr987!Z@ds121091.mlab.com:21091/up-skills');
-    const db = await connection.db('up-skills');
-    const collection = db.collection('links');
-    const id = await getNextSequence('userid', db);
-    const document = await collection.insertOne({ id, link });
-    res.send({ link: encode(id) });
+    try {
+        const { link } = req.body;
+        const connection = await MongoClient.connect('mongodb://Andrew:ANDr987!Z@ds121091.mlab.com:21091/up-skills');
+        const db = await connection.db('up-skills');
+        const collection = db.collection('links');
+        const id = await getNextSequence('userid', db);
+        await collection.insertOne({ id, link });
+        res.send({ link: `${host}:${port}/${encode(id)}` });
+    } catch (e) {
+        res.send({ e })
+    }
 });
 
 app.get('/:link', async (req, res) => {
@@ -37,11 +43,11 @@ app.get('/:link', async (req, res) => {
     const document = await collection.findOne({ id: decode(link) });
     console.log('document', document);
     if (document && document.link) {
-        res.redirect(`https://${document.link}`);
+        res.redirect(`${document.link}`);
     } else {
         res.send({ message: 'Could not find the page' })
     }
 });
 
 
-app.listen(port, () => console.log(`Listening to ${port}`));
+app.listen(port, host, () => console.log(`Listening to ${port}`));
