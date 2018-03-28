2c5f9ffd8fc52515f2dfe4a74b4be83ed7e91ed3
Andrey Lozhkin <las@htc-cs.ru>
2018-03-28 14:04:41 +0400

    Add comment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbConfig: 'mongodb://Andrew:ANDr987!Z@ds121091.mlab.com:21091/up-skills',
+    dbConfig: 'mongodb://Andrew:ANDr987!Z@ds121091.mlab.com:21091/up-skills', // Trash db. There is nothing important there.
     host: '127.0.0.1',
     port: 3000
 };
